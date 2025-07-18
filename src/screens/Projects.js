897aabd05acbc39974b55cf0a5c1d521087f703b
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-07-18 12:38:30 -0700

    added photos

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -11,20 +11,23 @@ function Projects() {
     }, []);
 
     return (
-        <section>
-            <h2>Projects I have worked on!</h2>
+        <div>
+            <h2 class="m-3">Some of my projects</h2>
+            <div class="card-deck">
             {projects.map((project, idx) => (
-                
-                <div key={idx} className="project"> 
-                    <div>
-                        <h3>{project.name}</h3>
-                        <p>{project.description}</p>
-                        <p>{project.date}</p>
+                <div class="card m-3 w-50" key={idx} className="project"> 
+                    <img class="card-img-top w-25" src={project.img} alt="Card img"/>
+                    <div class="card-body">
+                        <h3 class="card-title">{project.name}</h3>
+                        <p class="card-subtitle mb-2 text-muted">{project.date}</p>
+                        <p class="card-text">{project.description}</p>
+                        <a class="card-link" href={project.link}>Link</a> 
+                        {/* TODO: add check for links (some are empty) */}
                     </div>
-
                 </div>
             ))}
-        </section>
+            </div>
+        </div>
     );
 };
 
